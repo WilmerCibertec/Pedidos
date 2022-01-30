4cfc2371ce2940234f564c0617407dfa4f3a5cf6
WilmerCibertec <95490214+WilmerCibertec@users.noreply.github.com>
2022-01-30 13:15:14 -0500

    Add files via upload

---
+++ App.Pedidos.Models/Cliente.cs
@@ -0,0 +1,37 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Pedidos.Models
+{
+    [Table("Clientes")]
+    public class Cliente
+    {
+        [Key]
+        [DisplayName("Id")]
+        public int IdPersona { get; set; }
+
+        public string Nombres { get; set; }
+
+        public string Apellidos { get; set; }
+
+        public string Documento { get; set; }
+
+        public string Direccion { get; set; }
+        public string Distrito { get; set; }
+        public string Provincia { get; set; }
+        public string Telefono { get; set; }
+        public bool Estado { get; set; }
+
+        [DisplayName("Fecha Creacion")]
+        public DateTime? FechaCreacion { get; set; }
+
+        [DisplayName("Fecha Actualizacion")]
+        public DateTime? FechaActualizacion { get; set; }
+
+    }
+}
---
+++ App.Pedidos.Models/Familia.cs
@@ -0,0 +1,27 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Pedidos.Models
+{
+    [Table("ClaseFamilia")]
+    public class Familia
+    {
+        [ExplicitKey]
+        [DisplayName("Linea")]
+        public int IdLinea { get; set; }
+
+        [ExplicitKey]
+        [DisplayName("Familia")]
+        public int IdFamilia { get; set; }
+
+        [DisplayName("Descripcion")]
+        public string DescripcioLocal { get; set; }
+
+        public bool? Estado { get; set; }
+    }
+}
